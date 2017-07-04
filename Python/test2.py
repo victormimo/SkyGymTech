b86f65101e89ab0cbd9763f6f26fa68491def508
JaredWestreich <jared.westreich@gmail.com>
2017-07-04 12:39:06 -0400

    Updating the PinOutput arduino code and test2 python code (which grabs pin status from arduino and counts reps and sets

--- Python/test2.py
+++ Python/test2.py
@@ -5,57 +5,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 arduino = serial.Serial('COM5', 9600, timeout=.1)
-# vel_old = 1
-# rep = 0
-# set = 0
-# vel_list = []
 
 prev_A = 0
-prev_val_B = 0
-# long t_A, t_B, cur_t  #time variables
+prev_B = 0
+
 t_A = -0.0001
 t_B = -0.0001
+t_B_old = -0.0001
 radius = 0.04 # m
-way = -1;
+way = 0;
 reps = 0;
 oldway = 0;
 reptime = 0
-reptime2 = 0
-set = 0
+sets = 0
 A = 0
 B = 0
+
+def arduino_pin(letter, data_in):
+    if letter in data_in:
+        data_out = filter(lambda x: x.isdigit(), data)
+        data_out = float(data_out)
+        return data_out
+
+def rising_edge(prev, pres):
+    cur_t = time.clock()
+    return cur_t
 
 while True:
     data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
     if data:
-        if "A" in data:
-            A = filter(lambda x: x.isdigit(), data)
-            A = float(A)
-            print "A is: ", A
-        if "B" in data:
-            B = filter(lambda x: x.isdigit(), data)
-            B = float(B)
-            print "B is: ", B
-    if prev_A == 0 and A == 1:
-        cur_t = time.clock()
-        vel = way*2*3.14*0.04 / (cur_t - t_A)
-        t_A = cur_t
+
+        A_input = arduino_pin("A", data)
+        B_input = arduino_pin("B", data)
+        if A_input == 0 or A_input == 1:
+            # print time.time()
+            A = A_input
+            # print "A:"
+            # print A
+            # print prev_A
+        if B_input == 0 or B_input == 1:
+            B = B_input
+            # print "B:"
+            # print B
+            # print prev_B
+        if prev_A == 0 and A == 1:
+            t_A = time.clock()
         prev_A = A
+        if prev_B == 0 and B == 1:
+            t_B = time.clock()
+            if (t_A - t_B_old) < (t_B - t_A):
+                way = 1
+            else:
+                way = -1
+            vel = way * 2 * 3.14 * 0.04 / (t_B - t_B_old)
+            print vel
+            # print time.time()
+            t_B_old = t_B
+        prev_B = B
 
-    if prev_B == 0 and B == 1:
-        t_B = time.clock()
+        if way == -1 and oldway == 1:
+            reptime = time.time()
+            reps = reps + 1
+            print "sets:"
+            print sets
+            print "reps:"
+            print reps
+        oldway = way
 
-        # print vel
-        # if (vel > -7 and vel < 7):
-        #     if (vel_old<=0 and vel>0):
-        #         if (rep!=0):
-        #             if (time.time()-reptime > 10):
-        #                 rep = 0
-        #                 set = set +1
-        #         rep = rep +1
-        #         reptime = time.time()
-        #     vel_old = vel
-        #     vel_list.append(vel)
-        #     print rep
-        #     print vel_list
-        #     print len(vel_list)
+        if (time.time() - reptime) >= 20 and reps > 0:
+            reps = 0
+            sets = sets + 1
